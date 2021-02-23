e2ed915624b49eba4cd481ad61214330640253c2
Muhammet Çokyaman <cokyamanmuhammet@gmail.com>
2021-02-23 14:27:14 +0300

    mateiral-ui useStyles  how it get props useStyles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
 import Header from "./components/Header";
-import Content from "./components/Content"
+import Content from "./components/Content";
 
 const App = () => {
+  const colorChange = false;
   return (
     <Grid container direction="column">
-      <Header />
+      <Header colorChange={colorChange} />
       <Grid item container>
         <Grid item xs={false} sm={2} />
         <Grid item xs={12} sm={8}>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,34 @@
 import React from "react";
-import { AppBar, Button, IconButton, makeStyles, Toolbar, Typography } from "@material-ui/core";
-import AppleIcon from '@material-ui/icons/Apple';
+import { AppBar, makeStyles, Toolbar, Typography } from "@material-ui/core";
+import AppleIcon from "@material-ui/icons/Apple";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow:1,
-    },
-    title: {
-        flexGrow: 1,
-    }
-}))
+  root: {
+    flexGrow: 1
+  },
+  title: {
+    flexGrow: 1
+  },
+  iconStyles: {
+    color: (props) => (props.colorChange ? "blue" : "red")
+  }
+}));
 
-const Header = () => {
-    const classes = useStyles();
-    return (
-        <div className={classes.root}>
-            <AppBar position="static">
-                <Toolbar>
-                    <Typography variant="h6" className={classes.title}>
-                        Material-ui
-                    </Typography>
-                    <AppleIcon />
-                </Toolbar>
-            </AppBar>
-        </div>    
-    );
+const Header = (props) => {
+  const classes = useStyles(props);
+  console.log(classes);
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Material-ui
+          </Typography>
+          <AppleIcon className={classes.iconStyles} />
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 };
 
 export default Header;
